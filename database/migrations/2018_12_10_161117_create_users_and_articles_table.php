10b0b23252076f2721f1267bf15f85d84b018d7a
xiangqian <175023117@qq.com>
2018-12-10 17:40:33 +0800

    创建用户、文章、文章分类表

---
+++ database/migrations/2018_12_10_161117_create_users_and_articles_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersAndArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 用户表
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('mobile', 50)->comment('手机号');
+            $table->string('nickname',100)->comment('昵称');
+            $table->string('password')->comment('密码');
+            $table->timestamps();
+        });
+
+        // 文章分类表
+        Schema::create('article_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->comment('分类名');
+            $table->unsignedInteger('pid')->default(0)->comment('上级id');
+            $table->unsignedInteger('sort')->default(0)->comment('排序');
+            $table->timestamps();
+        });
+
+        // 文章表
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->comment('用户id');
+            $table->integer('type')->comment('类型');
+            $table->string('title', 50)->comment('标题');
+            $table->text('content')->comment('内容');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}

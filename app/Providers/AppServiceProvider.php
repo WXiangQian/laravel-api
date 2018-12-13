33c49df26e96ae5206f1373297533103a32db61e
xiangqian <175023117@qq.com>
2018-12-13 09:37:24 +0800

    输出SQL语句时保存到log日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \DB::listen(function ($query) {
+            $tmp = str_replace('?', '"'.'%s'.'"', $query->sql);
+            $qBindings = [];
+            foreach ($query->bindings as $key => $value) {
+                if (is_numeric($key)) {
+                    $qBindings[] = $value;
+                } else {
+                    $tmp = str_replace(':'.$key, '"'.$value.'"', $tmp);
+                }
+            }
+            $tmp = vsprintf($tmp, $qBindings);
+            $tmp = str_replace("\\", "", $tmp);
+            \Log::info(' execution time: '.$query->time.'ms; '.$tmp."\n\n\t");
+
+        }
+        );
     }
 
     /**

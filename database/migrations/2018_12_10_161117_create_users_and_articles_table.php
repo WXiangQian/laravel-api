68c4677a84fd4feef1300961102c9cff34d0457e
xiangqian <175023117@qq.com>
2018-12-21 10:22:12 +0800

    去除点赞量字段

--- database/migrations/2018_12_10_161117_create_users_and_articles_table.php
+++ database/migrations/2018_12_10_161117_create_users_and_articles_table.php
@@ -38,7 +38,6 @@ class CreateUsersAndArticlesTable extends Migration
             $table->integer('type_id')->comment('类型id');
             $table->string('title', 50)->comment('标题');
             $table->text('content')->comment('内容');
-            $table->integer('vote')->default(0)->comment('点赞量');
             $table->softDeletes();
             $table->timestamps();
         });

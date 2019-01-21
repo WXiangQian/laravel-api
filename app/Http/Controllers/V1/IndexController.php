12c9b79146845b6e2d6080358901073a06212f83
xiangqian <175023117@qq.com>
2019-01-21 10:44:55 +0800

    完善接口文档

--- app/Http/Controllers/V1/IndexController.php
+++ app/Http/Controllers/V1/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\V1;
 
+use App\Transformers\ExpressTransformer;
 use App\Transformers\UserInfoTransformer;
 use Illuminate\Http\Request;
 use Wythe\Logistics\Logistics;
@@ -40,6 +41,46 @@ class IndexController extends BaseController
         return $this->responseData(UserInfoTransformer::transform($data));
     }
 
+    /**
+     * @SWG\Post(
+     *      path="/express",
+     *      tags={"public"},
+     *      operationId="express",
+     *      summary="查询快递信息",
+     *      consumes={"application/json"},
+     *      produces={"application/json"},
+     *     @SWG\Parameter(
+     *          in="body",
+     *          name="data",
+     *          description="",
+     *          required=true,
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(property="code",description="快递单号",type="string"),
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(property="code", type="string",description="状态码"),
+     *              @SWG\Property(property="message", type="string",description="提示信息"),
+     *              @SWG\Property(property="data", type="object",
+     *                  @SWG\Property(property="status", type="string", description="201单号不存在或者已经过期；200表示请求成功"),
+     *                  @SWG\Property(property="message", type="string", description="当status为201时，info会返回具体错误原因，否则返回“OK”"),
+     *                  @SWG\Property(property="logistics_company", type="string", description="具体哪家快递"),
+     *                  @SWG\Property(property="data", type="array",description="快递详情",
+     *                      @SWG\Items(type="object",
+     *                          @SWG\Property(property="time", type="string",description="时间"),
+     *                          @SWG\Property(property="description", type="string",description="快递进度"),
+     *                      ),
+     *                  ),
+     *              ),
+     *          )
+     *      ),
+     * )
+     */
     public function express(Request $request)
     {
         $code = $request->input('code', '');
@@ -49,6 +90,6 @@ class IndexController extends BaseController
         $logistics = new Logistics();
 
         $data = $logistics->query($code, 'kuaidi100');
-        return $this->responseData($data);
+        return $this->responseData(ExpressTransformer::transform($data));
     }
 }

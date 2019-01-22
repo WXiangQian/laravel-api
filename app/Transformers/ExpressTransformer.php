27a536efbfa673d555420de21fd3d78ce2b75659
xiangqian <175023117@qq.com>
2019-01-22 10:27:42 +0800

    完善快递接口

--- app/Transformers/ExpressTransformer.php
+++ app/Transformers/ExpressTransformer.php
@@ -7,7 +7,18 @@ class ExpressTransformer extends BaseTransformer
 {
     public static function transform($item)
     {
-        return $item['kuaidi100']['result'];
+        if (isset($item['kuaidi100']['result'])) {
+            return $item['kuaidi100']['result'];
+        } else {
+            return [
+                'status' => 201,
+                'message' => '快递公司参数异常：单号不存在或者已经过期',
+                'error_code' => 0,
+                "data" => [],
+                "logistics_company" => "",
+                "logistics_bill_no" => ""
+            ];
+        }
     }
 
 

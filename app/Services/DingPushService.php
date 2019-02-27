9ee8d3368f26047f59b8d32f5301f8a7e883a4d8
xiangqian <175023117@qq.com>
2019-02-27 11:32:02 +0800

    添加注释

--- app/Services/DingPushService.php
+++ app/Services/DingPushService.php
@@ -8,6 +8,11 @@ use Qian\DingTalk\Message;
 
 class DingPushService
 {
+    /**
+     * 推送text消息到钉钉群
+     * @param string $content 推送内容
+     * @return string
+     */
     public static function sendText($content = ' ')
     {
         $DingTalk = new DingTalk();
@@ -18,6 +23,14 @@ class DingPushService
         return $res;
     }
 
+    /**
+     * 推送link消息到钉钉群
+     * @param string $title 标题
+     * @param string $text  内容
+     * @param string $messageUrl   要打开的链接
+     * @param string $picUrl    图片链接
+     * @return string
+     */
     public static function sendLink($title = ' ', $text = ' ', $messageUrl, $picUrl = '')
     {
         $DingTalk = new DingTalk();

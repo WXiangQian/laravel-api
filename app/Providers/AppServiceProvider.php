a7ee0076679c5d1eefc881a7853d6b0971c32c3f
xiangqian <175023117@qq.com>
2019-04-29 14:42:15 +0800

    修复创建数据表时报错问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         \DB::listen(function ($query) {
             $tmp = str_replace('?', '"' . '%s' . '"', $query->sql);
             $qBindings = [];

ff70ad63a921bcba537b48594a35b5909569a7cd
xiangqian <175023117@qq.com>
2019-08-26 10:43:11 +0800

    create jobs文件

--- app/Jobs/DemoJob.php
+++ app/Jobs/DemoJob.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Log;
 
-class Demo implements ShouldQueue
+class DemoJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
